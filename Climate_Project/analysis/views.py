89f076d705f798e1916b60c403011846821a86c3
subashpoudel024 <paudelsubash1234@gmail.com>
2024-05-24 17:44:15 +0545

    trained with prophed model

--- Climate_Project/analysis/views.py
+++ Climate_Project/analysis/views.py
@@ -6,7 +6,7 @@ import numpy as np
 import __main__
 __main__.pd = pd
 import dill
-
+import prophet
 
 # Create your views here.
 
@@ -15,18 +15,46 @@ def feature_entry(request):
     if request.method == 'POST':
                 # Retrieve form data
         date = request.POST.get('DATE')
+        ps = float(request.POST.get('PS'))
+        qv2m = float(request.POST.get('QV2M'))
+        ws10m = float(request.POST.get('WS10M'))
+
+                
+        # Initialize a DataFrame with a single row
+        future = pd.DataFrame({'ds': [date]})
+        # Convert the 'ds' column to datetime
+        future['ds'] = pd.to_datetime(future['ds'])
+
+        # Extract year, month, and day from the 'ds' column
+        future['YEAR'] = future['ds'].dt.year
+        future['MONTH'] = future['ds'].dt.month
+        future['DAY'] = future['ds'].dt.day
+
+        # Add other columns with constant values
+        future['PS'] = ps
+        future['QV2M'] = qv2m
+        future['WS10M'] = ws10m
 
 
         
 
-        forecast_date = date
-        forecast_date=pd.Timestamp(forecast_date)
+        # forecast_date = date
+        # forecast_date=pd.Timestamp(forecast_date)
 
-        with open('analysis/Model/exp_model.pkl', 'rb') as f:
+        with open('analysis/Model/fb-prophet_model.pkl', 'rb') as f:
             model = dill.load(f)
-        y_pred = model.predict(forecast_date)
-        print(y_pred)
-        return render(request,'analysis/result.html',{'y_pred':round(y_pred[0],2)})
+        forecast = model.predict(future)
+
+        forecast_final = forecast[['yhat','yhat_lower','yhat_upper']]
+        # Rename the columns
+        forecast_final.columns = ['Average-Forecast', 'Minimum-Forecast', 'Maximum-Forecast']
+        print('Forecast:', forecast_final)
+        print(type(forecast_final))
+
+        return render(request,'analysis/result.html',{'avg': round(forecast_final['Average-Forecast'].iloc[0],2),
+                                                      'min': round(forecast_final['Minimum-Forecast'].iloc[0],2),
+                                                      'max': round(forecast_final['Maximum-Forecast'].iloc[0],2)
+                                                      })
     return render(request, 'analysis/feature_entry.html')
 
 #  <=============================== YOU CAN IGNORE THE BELOW COMMENTED CODE ==================================>
